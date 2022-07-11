7689cb49d3b16c171ac177d63b26fcae2b034dad
GeorgiIvanov <Georgi.Ivanov@paynetics.digital>
2022-07-11 10:14:13 +0300

    WIP

--- tests/api/LS/AssociateGroupCardinalCest.php
+++ tests/api/LS/AssociateGroupCardinalCest.php
@@ -10,13 +10,15 @@ use function PHPUnit\Framework\assertJsonStringEqualsJsonString;
 use function PHPUnit\Framework\assertNotNull;
 
 /** Tests if user is able to associate group to instance and/or merchant or user
- * @group associate
+ * @group associateandremove
  */
 class AssociateGroupCardinalCest extends CardsBase
 {
 
-
-    public function associateGroupInstance(ApiTester $I)
+    /** Tests if user is able to associate group to instance and/or merchant or user
+     * @group associateandel
+     */
+    public function associateGroupInstanceAndThenDelete(ApiTester $I)
     {
         //create group
         $this->ensureGroup($I);
@@ -37,13 +39,22 @@ class AssociateGroupCardinalCest extends CardsBase
         $this->checkDefaultResponse($I);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
+
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/$this->testGROUPToken/associate", ([
+            "instance" => $rand_str
+        ]));
+
+        $this->checkDefaultResponse($I);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
         //dd($details);
 
 
 //        var_dump($I->grabResponse());
     }
 
-    public function associateGroupInstanceAndMerchant(ApiTester $I)
+    public function associateGroupInstanceAndMerchantThenDelete(ApiTester $I)
     {
         //create group
         $this->ensureGroup($I);
@@ -69,6 +80,18 @@ class AssociateGroupCardinalCest extends CardsBase
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
         //dd($details);
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/$this->testGROUPToken/associate", ([
+            "instance" => $rand_str,
+            "merchant" => $rand_str2
+        ]));
+
+        $this->checkDefaultResponse($I);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        //dd($details);
+
+
 
 
 //        var_dump($I->grabResponse());
@@ -101,10 +124,21 @@ class AssociateGroupCardinalCest extends CardsBase
         $I->seeResponseIsJson();
         //dd($details);
 
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/$this->testGROUPToken/associate", ([
+            "instance" => $rand_str,
+            "user" => $rand_str2
+        ]));
 
+        $this->checkDefaultResponse($I);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
 //        var_dump($I->grabResponse());
     }
 
+    /** Check if user is able to make association for instance/merchant/user
+     * @group associationAll
+     */
     public function associateGroupInstanceAndUserAndMerchant(ApiTester $I)
     {
         //create group
@@ -135,7 +169,16 @@ class AssociateGroupCardinalCest extends CardsBase
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
         //dd($details);
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/$this->testGROUPToken/associate", ([
+            "instance" => $rand_str,
+            "user" => $rand_str2,
+            "merchant" => $rand_str3
+        ]));
 
+        $this->checkDefaultResponse($I);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
 
 //        var_dump($I->grabResponse());
     }
@@ -177,7 +220,7 @@ class AssociateGroupCardinalCest extends CardsBase
 //        var_dump($I->grabResponse());
     }
 
-    public function associateGroupWithUnexistingLimitGroupNS(ApiTester $I)
+    public function associateGroupAlreadyExistingLimitGroupNS(ApiTester $I)
     {
         //create group
         $this->ensureGroup($I);
@@ -206,6 +249,90 @@ class AssociateGroupCardinalCest extends CardsBase
 
 //        var_dump($I->grabResponse());
     }
+
+    /** Negative scenarios for remove association fon instance/merchant/user
+     * @group removeAssociationNS
+     */
+    public function removeAassociationGroupNotFound(ApiTester $I)
+    {
+        //create group
+        $this->ensureGroup($I);
+
+
+
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/123456789/associate", ([
+            "instance" => "testInstance"
+        ]));
+
+        $this->checkDefaultResponse($I, 7002);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        //dd($details);
+        $I->seeResponseMatchesJsonType([
+
+            'code' => 'integer',
+            'message' => 'string'
+
+
+        ]);
+        $details = $I->grabDataFromResponseByJsonPath('$.message.')[0]; //0 is the first element
+        assertEquals($details, 'services.limit.group_not_found');
+        assertNotNull($details);
+        print_r($details);
+
+    }
+
+    /** Negative scenarios for remove association fon instance/merchant/user
+     * @group removeAssociationNS2
+     */
+    public function removeAssociationNotFound(ApiTester $I)
+    {
+        //create group
+        $this->ensureGroup($I);
+
+        //create random string for unique name to be updated every time
+        $charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $rand_str = '';
+        $desired_length = 10;
+        while (strlen($rand_str) < $desired_length)
+            $rand_str .= substr(str_shuffle($charset), 0, 1);
+
+
+        //this should be successful
+        $I->sendPost("/v1/groups/$this->testGROUPToken/associate", json_encode([
+            "instance" => $rand_str
+        ]));
+
+        $this->checkDefaultResponse($I);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+
+        //try to successfully remove created association
+        $I->sendDelete("/v1/groups/$this->testGROUPToken/associate", ([
+            "instance" => "testInstance"
+        ]));
+
+        $this->checkDefaultResponse($I, 7007);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        //dd($details);
+        $I->seeResponseMatchesJsonType([
+
+            'code' => 'integer',
+            'message' => 'string'
+
+
+        ]);
+        $details = $I->grabDataFromResponseByJsonPath('$.message.')[0]; //0 is the first element
+        assertEquals($details, 'services.limit.association_not_found');
+        assertNotNull($details);
+        print_r($details);
+
+    }
+
+//        var_dump($I->grabResponse());
+
 
 //    public function associateGroupWithDeletedLimitGroupNS(ApiTester $I)
 //    {
